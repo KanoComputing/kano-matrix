0747b0d3c094811db2ca847b92f1c393d857318b
Radek Pazdera <radek@kano.me>
2015-02-13 22:03:22 +0000

    Replacing the Chinese characters with a different set
    This commit replaces the Chinese characters that were previously chosen
    at random by a set that is less likely to suggest meaning.
    
    Suggested-by: Yao Wei <mwei@lxde.org>

--- src/drop.js
+++ src/drop.js
@@ -1,7 +1,7 @@
 var Drop = function (x, canvas, ctx, fontSize) {
     /* A pool of characters that are used for the threads */
     var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" +
-                  "畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑";
+                  "甲乙丙丁戊己庚辛壬癸子丑寅卯辰巳午未申酉戌亥";
     this.charset = charset.split("");
 
     this.canvas = canvas;
